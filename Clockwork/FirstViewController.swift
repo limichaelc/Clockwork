c63183721a57f85525ec3e5d9ecea801094be163
limichaelc <li.michael.c@gmail.com>
2014-07-19 12:49:50 -0700

    Initial Commit

---
+++ Clockwork/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Clockwork
+//
+//  Created by Michael Li on 7/19/14.
+//  Copyright (c) 2014 The Traveling Salespeople. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ClockworkTests/ClockworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  ClockworkTests.swift
+//  ClockworkTests
+//
+//  Created by Michael Li on 7/19/14.
+//  Copyright (c) 2014 The Traveling Salespeople. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ClockworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
