60b69bcd606ae54823c56058a298b82877ba1979
limichaelc <li.michael.c@gmail.com>
2014-07-19 15:52:14 -0700

    Edits to Task

--- Clockwork/Task.swift
+++ Clockwork/Task.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 import EventKit
 import EventKitUI
+import MapKit
 
 class Task {
     enum Priority {
@@ -22,18 +23,30 @@ class Task {
     
     // Properties
     var name: String
-    var events: [EKEvent] // array of all allocated chunks of time
+    var color: Color
+    var chunkable: Bool
+    var priority: Priority
+//    var duration: NSTimeInterval?
+//    var location: MKMapItem?
+    
+    // array of all allocated chunks of time
+    // populated based on the settings provided above
     /* contains properties for:
         title
         startDate and endDate
         associated calendar
         alarms
         recurrenceRules */
+//    var events: [EKEvent]
     
-    var location: [MKMapItem]
-    var color: Color
-    var priority: Priority
-    var duration: NSTimeInterval
-    var chunkable: Bool
-    
+    init() {
+        name = "temp"
+        color = .Gray
+        chunkable = false
+        priority = .Medium
+//        duration = NSTimeInterval(10)
+//        location = nil
+//        events = EKEvent(
+        
+    }
 }
